c6025bdb6e5efca33b32061fa17add20aff53f11
Binh <binhthaps36193@fpt.edu.vn>
2024-10-17 21:21:35 +0700

    shipper cho hùng

--- controllers/shipper/ModelShipper.js
+++ controllers/shipper/ModelShipper.js
@@ -3,21 +3,21 @@ const Schema = mongoose.Schema;
 const OrderSchema = require('../order/ModelOrder');
 
 const ShipperSchema = new Schema({
-  name: { type: String, required: true },
+  name: { type: String, required: false },
   phone: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  address: { type: String, required: true },
-  rating: { type: String, required: true },
+  address: { type: String, required: false },
+  rating: { type: Number, required: false },
   role: { type: String, enum: ['shipper'], default: 'shipper' },
   status: { type: String, enum: ['active', 'inactive'], default: 'active' },
-  image: { type: Array, required: true, default: [] },
+  image: { type: Array, required: false, default: [] },
   password: { type: String, required: true },
   created_at: { type: Date, default: Date.now },
   updated_at: { type: Date, default: Date.now },
   gender: { type: String, enum: ['male', 'female', 'other'], required: true }, // Giới tính
-  birthDate: { type: Date, required: true }, // Ngày sinh
-  vehicleBrand: { type: String, required: true }, // Hãng xe
-  vehiclePlate: { type: String, required: true } // Biển số xe
+  birthDate: { type: Date, required: false }, // Ngày sinh
+  vehicleBrand: { type: String, required: false }, // Hãng xe
+  vehiclePlate: { type: String, required: false } // Biển số xe
 });
 
 module.exports = mongoose.models.Shipper || mongoose.model('Shipper', ShipperSchema);
