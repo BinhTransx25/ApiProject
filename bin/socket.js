665b6e1e1efa5c387c06411f72eb0ee4d9fbdedf
ɭê ɦùռɠ <125887825+nguoilaisaochoi@users.noreply.github.com>
2024-10-29 09:22:34 +0700

    29 -10 -24 xgvxv

--- bin/socket.js
+++ bin/socket.js
@@ -1,8 +1,8 @@
 module.exports = function (io) {
-    io.on('connection', (socket) => {
-        console.log('A client connected', socket.id);
+  io.on("connection", (socket) => {
+    console.log("A client connected", socket.id);
 
-        /** 
+    /** 
         // Khi shipper hoặc user tham gia vào một room (phòng chat cho đơn hàng)
         socket.on('join_room', async ({ roomId, userId, shipperId }) => {
             try {
@@ -28,96 +28,107 @@ module.exports = function (io) {
             console.log(`Message from ${senderId} in room ${roomId}: ${message}`);
         });
         */
-        // Khi shipper hoặc user tham gia vào một room (phòng chat cho đơn hàng)
-        socket.on("join_room", async (roomID) => {
-            socket.join(roomID);
-            //console.log(`Client ${socket.id} joined room ${roomID}`);
-        });
-
-        // Nhận tin nhắn từ client và phát lại cho các client khác trong cùng room
-        socket.on("send_message", ({ roomID, data }) => {
-            //console.log('Room ID received:', roomID); 
-            io.to(roomID).emit("receive_message", data);
-            //console.log(`Message from ${data.name}: ${data.text}`);
-        });
-        // Khi có đơn hàng mới được tạo và bắn socket tới shopOwner
-        socket.on('order_created', (orderData) => {
-            const shopOwnerId = orderData.shopOwnerId; // Lấy ID của cửa hàng từ orderData
-
-            // Gửi thông báo đến room của cửa hàng tương ứng
-            io.to(shopOwnerId).emit('new_order_created', orderData);
-            console.log(`New order created for shop ${shopOwnerId}: ${orderData.orderId}`);
-        });
-
-        // ShopOwner Xác nhận đơn hàng
-        socket.on('confirm_order', async (orderId) => {
-            try {
-                const order = await confirmOrder(orderId, io); // Gọi hàm confirmOrder từ controller
-                socket.emit('order_confirmed', { orderId, status: order.status });
-            } catch (error) {
-                socket.emit('error', { message: error.message });
-                console.error('Error confirming order:', error);
-            }
-        });
-
-        // ShopOwner Hủy đơn hàng
-        socket.on('cancel_order', async (orderId) => {
-            try {
-                const order = await shopOwnerCancelOrder(orderId, io); // Gọi hàm shopOwnerCancelOrder từ controller
-                socket.emit('order_cancelled', { orderId, status: order.status });
-            } catch (error) {
-                socket.emit('error', { message: error.message });
-                console.error('Error cancelling order:', error);
-            }
-        });
-
-        // Hủy đơn hàng (Customer)
-        socket.on('customer_cancel_order', async (orderId) => {
-            try {
-                const order = await CustomerCancelOrder(orderId, io); // Gọi hàm CustomerCancelOrder từ controller
-                socket.emit('order_cancelled', { orderId, status: order.status });
-            } catch (error) {
-                socket.emit('error', { message: error.message });
-                console.error('Error cancelling order by customer:', error);
-            }
-        });
-
-        // Xác nhận đơn hàng có shipper
-        socket.on('confirm_order_shipper_exists', async ({ orderId, shipperId }) => {
-            try {
-                const order = await confirmOrderShipperExists(orderId, shipperId);
-                socket.emit('order_confirmed', { orderId, status: order.status });
-            } catch (error) {
-                socket.emit('error', { message: error.message });
-                console.error('Error confirming order for shipper:', error);
-            }
-        });
-
-        // Xác nhận đơn hàng theo shipper ID
-        socket.on('confirm_order_by_shipper_id', async ({ orderId, shipperId }) => {
-            try {
-                const order = await confirmOrderByShipperId(orderId, shipperId);
-                socket.emit('order_confirmed', { orderId, status: order.status });
-            } catch (error) {
-                socket.emit('error', { message: error.message });
-                console.error('Error confirming order by shipper ID:', error);
-            }
-        });
-
-        // Hủy đơn hàng theo shipper ID
-        socket.on('cancel_order_by_shipper_id', async ({ orderId, shipperId }) => {
-            try {
-                const order = await cancelOrderByShipperId(orderId, shipperId);
-                socket.emit('order_cancelled', { orderId, status: order.status });
-            } catch (error) {
-                socket.emit('error', { message: error.message });
-                console.error('Error cancelling order by shipper ID:', error);
-            }
-        });
-
-        // Khi client ngắt kết nối
-        socket.on('disconnect', () => {
-            console.log('A client disconnected', socket.id);
-        });
+    // Khi shipper hoặc user tham gia vào một room (phòng chat cho đơn hàng)
+    socket.on("join_room", async (roomID) => {
+      socket.join(roomID);
+      //console.log(`Client ${socket.id} joined room ${roomID}`);
     });
+
+    // Nhận tin nhắn từ client và phát lại cho các client khác trong cùng room
+    socket.on("send_message", ({ roomID, data }) => {
+      //console.log('Room ID received:', roomID);
+      io.to(roomID).emit("receive_message", data);
+      //console.log(`Message from ${data.name}: ${data.text}`);
+    });
+    // Khi có đơn hàng mới được tạo và bắn socket tới shopOwner
+    socket.on("order_created", (orderData) => {
+      const shopOwnerId = orderData.shopOwnerId; // Lấy ID của cửa hàng từ orderData
+
+      // Gửi thông báo đến room của cửa hàng tương ứng
+      io.to(shopOwnerId).emit("new_order_created", orderData);
+      console.log(
+        `New order created for shop ${shopOwnerId}: ${orderData.orderId}`
+      );
+    });
+
+    // Khi có đơn hàng mới được tạo và bắn socket tới shipper
+    socket.on("order_shipper", (Data) => {
+      io.emit("order_shipper_receive", Data);
+      console.log(Data);
+    });
+
+    // ShopOwner Xác nhận đơn hàng
+    socket.on("confirm_order", async (orderId) => {
+      try {
+        const order = await confirmOrder(orderId, io); // Gọi hàm confirmOrder từ controller
+        socket.emit("order_confirmed", { orderId, status: order.status });
+      } catch (error) {
+        socket.emit("error", { message: error.message });
+        console.error("Error confirming order:", error);
+      }
+    });
+
+    // ShopOwner Hủy đơn hàng
+    socket.on("cancel_order", async (orderId) => {
+      try {
+        const order = await shopOwnerCancelOrder(orderId, io); // Gọi hàm shopOwnerCancelOrder từ controller
+        socket.emit("order_cancelled", { orderId, status: order.status });
+      } catch (error) {
+        socket.emit("error", { message: error.message });
+        console.error("Error cancelling order:", error);
+      }
+    });
+
+    // Hủy đơn hàng (Customer)
+    socket.on("customer_cancel_order", async (orderId) => {
+      try {
+        const order = await CustomerCancelOrder(orderId, io); // Gọi hàm CustomerCancelOrder từ controller
+        socket.emit("order_cancelled", { orderId, status: order.status });
+      } catch (error) {
+        socket.emit("error", { message: error.message });
+        console.error("Error cancelling order by customer:", error);
+      }
+    });
+
+    // Xác nhận đơn hàng có shipper
+    socket.on(
+      "confirm_order_shipper_exists",
+      async ({ orderId, shipperId }) => {
+        try {
+          const order = await confirmOrderShipperExists(orderId, shipperId);
+          socket.emit("order_confirmed", { orderId, status: order.status });
+        } catch (error) {
+          socket.emit("error", { message: error.message });
+          console.error("Error confirming order for shipper:", error);
+        }
+      }
+    );
+
+    // Xác nhận đơn hàng theo shipper ID
+    socket.on("confirm_order_by_shipper_id", async ({ orderId, shipperId }) => {
+      try {
+        const order = await confirmOrderByShipperId(orderId, shipperId);
+        socket.emit("order_confirmed", { orderId, status: order.status });
+      } catch (error) {
+        socket.emit("error", { message: error.message });
+        console.error("Error confirming order by shipper ID:", error);
+      }
+    });
+
+    // Hủy đơn hàng theo shipper ID
+    socket.on("cancel_order_by_shipper_id", async ({ orderId, shipperId }) => {
+      try {
+        const order = await cancelOrderByShipperId(orderId, shipperId);
+        socket.emit("order_cancelled", { orderId, status: order.status });
+      } catch (error) {
+        socket.emit("error", { message: error.message });
+        console.error("Error cancelling order by shipper ID:", error);
+      }
+    });
+
+    // Khi client ngắt kết nối
+    socket.on("disconnect", () => {
+      console.log("A client disconnected", socket.id);
+    });
+  });
 };
