643099a1d3327e9f273c8d65f66ecab654be8e06
gitt240 <nguyenminhkhuong318@gmail.com>
2024-12-01 21:40:27 +0700

    update

---
+++ routes/ZaloPay.js
@@ -0,0 +1,53 @@
+const axios = require('axios').default; // npm install axios
+const CryptoJS = require('crypto-js'); // npm install crypto-js
+const express = require('express'); // npm install express
+const bodyParser = require('body-parser'); // npm install body-parser
+const moment = require('moment'); // npm install moment
+const router = express.Router();
+
+const config = {
+    appid: 2554,
+    key1: 'sdngKKJmqEMzvh5QQcdD2A9XBSKUNaYn',
+    key2: 'trMrHtvjo6myautxDUiAcYsVtaeQ8nhf',
+    endpoint: 'https://sb-openapi.zalopay.vn/v2/create',
+};
+
+router.post('/callback', (req, res) => {
+    let result = {};
+    console.log(req.body);
+    try {
+      let dataStr = req.body.data;
+      let reqMac = req.body.mac;
+  
+      let mac = CryptoJS.HmacSHA256(dataStr, config.key2).toString();
+      console.log('mac =', mac);
+  
+      // kiểm tra callback hợp lệ (đến từ ZaloPay server)
+      if (reqMac !== mac) {
+        // callback không hợp lệ
+        result.return_code = -1;
+        result.return_message = 'mac not equal';
+      } else {
+        // thanh toán thành công
+        // merchant cập nhật trạng thái cho đơn hàng ở đây
+        let dataJson = JSON.parse(dataStr, config.key2);
+        console.log(
+          "update order's status = success where app_trans_id =",
+          dataJson['app_trans_id'],
+        );
+  
+        result.return_code = 1;
+        result.return_message = 'success';
+      }
+    } catch (ex) {
+      console.log('lỗi:::' + ex.message);
+      result.return_code = 0; // ZaloPay server sẽ callback lại (tối đa 3 lần)
+      result.return_message = ex.message;
+    }
+  
+    // thông báo kết quả cho ZaloPay server
+    res.json(result);
+  });
+
+module.exports = router;
+
